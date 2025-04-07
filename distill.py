492c11a4300616f7f24857b5f09cdb4b04100684
Comus Leong <comus.leong@gmail.com>
2025-04-08 01:36:50 +0800

    update

--- distill.py
+++ distill.py
@@ -24,12 +24,12 @@ student_model.train(
     data="coco-pose.yaml",
     teacher=teacher_model.model,
     distillation_loss="mgd",              # 對不同通道數模型的蒸餾效果更好
-    distillation_layers=["16", "19", "22"], # P3, P4, P5特徵層全部使用
+    distillation_layers=["22"], # P3, P4, P5特徵層全部使用
     epochs=10,                            # 5萬+張數據，10個epoch已足夠
     imgsz=640,
     batch=64,                             # 大批次提高訓練效率
     workers=12,                           # 充分利用多核CPU
-    lr0=0.001,                            # 提高學習率以加速收斂
+    lr0=0.0001,                            # 提高學習率以加速收斂
     lrf=0.01,                             # 學習率衰減到初始的1%
     optimizer="Adam",                     # Adam優化器適合蒸餾任務
     cos_lr=True,                          # 使用餘弦學習率調度
@@ -44,7 +44,7 @@ student_model.train(
     # plots=True,                           # 生成訓練圖表，方便分析
     close_mosaic=0,                       # 禁用mosaic增強，蒸餾不需要太多增強
     augment=False,                        # 關閉增強，保持特徵對齊
-    distill=0.1,                          # 提高蒸餾權重，增強知識傳遞
-    fraction=0.3,                         # 增加訓練數據比例
+    distill=0.01,                          # 提高蒸餾權重，增強知識傳遞
+    fraction=0.1,                         # 增加訓練數據比例
     amp=False,                            # 關閉混合精度訓練以提高精度
 )
