844565652dfbe73a36a8e403e31651523480f737
Comus Leong <comus.leong@gmail.com>
2025-04-14 05:24:44 +0800

    update

--- train_yolo11n_pose_stage2.py
+++ train_yolo11n_pose_stage2.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo11n-pose/weights/last.pt")
+model = YOLO("yolo11n-pose/train/weights/last.pt")
 
 # 以30 epoch的checkpoint為基礎的二次訓練參數
 results = model.train(
