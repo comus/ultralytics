1a30f6d64a0afc0105635187b923aa47f579cca1
Comus Leong <comus.leong@gmail.com>
2025-04-15 02:08:15 +0800

    update

--- train_pure_distill.py
+++ train_pure_distill.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # 載入新模型
-model = YOLO("yolo11n-pose-pure-distill/train/weights/epoch10.pt")
+model = YOLO("yolo11n-pose-pure-distill/train/weights/epoch8.pt")
 
 # 訓練模型 - 快速60 epochs
 results = model.train(
