b37c09c085612b08e9e7e3690901bcadc8fd7dc4
Comus Leong <comus.leong@gmail.com>
2025-04-18 11:50:10 +0800

    update

--- val2.py
+++ val2.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("distill_pose/yolo11n_distill_ultraconservative/weights/best.pt")
+model = YOLO("distill_pose/yolo11n_distill_ultraconservative/weights/last.pt")
 
 metrics = model.val(
   data="coco-pose.yaml",
