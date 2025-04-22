ad25f50b0872bc10f575d38fde213193c67ec694
Comus Leong <comus.leong@gmail.com>
2025-04-23 02:59:04 +0800

    update

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Initialize a new model from yaml configuration without pretrained weights
-model = YOLO("yolo11n-pose.yaml", pretrained=False)
+model = YOLO("yolo11n-pose.yaml")
 
 # Train the model with specified parameters
 results = model.train(
