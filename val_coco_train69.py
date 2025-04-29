0db6842e18a9501c3985cfc3c3a9cea932852780
Comus Leong <comus.leong@gmail.com>
2025-04-30 03:36:42 +0800

    update

--- val_coco_train69.py
+++ val_coco_train69.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("/root/autodl-tmp/ultralytics/runs/pose/train72/weights/best.pt")
+model = YOLO("/root/autodl-tmp/ultralytics/runs/pose/train69/weights/best.pt")
 
 # Validate the model
 metrics = model.val(
